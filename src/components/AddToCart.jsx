a817b0f2a92e9d87c916fcd853db04229e0811ce
Helflaa <146756162+Helflaa@users.noreply.github.com>
2025-03-11 15:01:19 +0100

    feat: added cart store
    added cart store using zustand and persist to store items in locale storages

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import {useCartStore} from "../store/cartStore";
+import {buttonStyles} from "./ButtonStyle";
+
---
+++ src/store/cartStore.jsx
@@ -0,0 +1,69 @@
+import {create} from "zustand";
+import {persist} from "zustand/middleware/persist";
+
+export const useCartStore = create(
+    persist(
+        (set, get) =>({
+            cart: [],
+
+            addToCart: (product) =>
+                set ((state) => {
+                    const existingProduct = state.cart.find((item) => item.id ===product.id)
+
+                    if (existingProduct){
+                        return {
+                            cart: state.cart.map((item) =>
+                            item.id === product.id
+                                ? {...item, quantity: item.quantity + 1 }
+                                : item
+                            ),
+                        };
+                    }
+
+                    return {
+                        cart: [...state.cart, {...product, quantity: 1}],
+                    };
+                }),
+
+            removeFromCart: (productId) =>
+                set((state)=>({
+                        cart:state.cart.filter((item)=> item.id !==productId),
+                    }
+                )),
+
+            itemIncrement: (productId) =>
+                set((state) => ({
+                    cart: state.cart.map((item)=>
+                     item.id === productId ? { ...item, quantity: item.quantity + 1 }: item
+                    ),
+                })),
+
+            itemDecrement: (productId) =>
+                set((state)=>({
+                    cart: state.cart
+                        .map((item)=>
+                            item.id ===productId
+                            ? {...item, quantity: item.quantity - 1 }
+                                : item
+                        )
+                        .filter((item) => item.quantity > 0),
+                })),
+
+            getTotalPrice: () => {
+                const { cart} = get();
+                return cart.reduce(
+                    (total,item) => total + item.quantity * (item.discountedPrice || item.price),
+                    0
+                );
+            },
+
+            clearCart: () => set ({ cart: [] }),
+
+        }),
+
+        {
+            name: "cart-storage",
+            getStorage: () =>localStorage,
+        },
+    ),
+);
