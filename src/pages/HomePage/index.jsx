d7da4e6dec9d159c92f7ca6f86e3911f1653ed20
Helflaa <146756162+Helflaa@users.noreply.github.com>
2025-03-12 15:47:32 +0100

    feat: added search function

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
-import { useProductStore } from "../../store/productsStore";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useProductStore} from "../../store/productsStore";
+import {Link} from "react-router-dom";
 import {ProductCard} from "../../components/ProductCard";
+import {Search} from "../../components/Search";
 
 export default function HomePage() {
-    const { products, loading, error, fetchProducts } = useProductStore();
+    const {products, loading, error, fetchProducts} = useProductStore();
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
         fetchProducts();
     }, [fetchProducts]);
+
+    useEffect(() => {
+        setSearchResults(products);
+    }, [products]);
 
     if (loading) {
         return <div className="text-center text-lg font-semibold">Loading...</div>;
@@ -18,13 +24,25 @@ export default function HomePage() {
     }
 
     return (
-        <div className="max-w-6xl mx-auto p-6 bg-white shadow-lg rounded-lg">
-            <h1 className="text-3xl font-bold text-center mb-6">Products</h1>
-            <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-4">
-                {products.map((product) => (
-                   <ProductCard product={product} key={product.id} />
-                ))}
+        <>
+            <div>
+                <Search setResults={setSearchResults}/>
             </div>
-        </div>
-    );
-}
+
+            <div className="max-w-6xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+                <h1 className="text-3xl font-bold text-center mb-6">Products</h1>
+                <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-4">
+                    {searchResults.length > 0 ? (
+                        searchResults.map((product) => (
+                            <ProductCard product={product} key={product.id} />
+                        ))
+                    ) : (
+                        <div className="col-span-full text-center text-gray-500">
+                            No results found.
+                        </div>
+                    )}
+                </div>
+            </div>
+            </>
+            );
+            }
